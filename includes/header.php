569e8927f036a79b8ec44c817e4b16f1bd65bd2f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-08 02:25:12 +0400

    add

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,75 @@
-<?php include './includes/header.css' ?>
-   <!--* верхний header с логин регистрацией и языком и логотипом-->
-    <div class="header__wrapper">
-      <!--container UP - LOGO / LOGIN / REgistration / LAnguage-->
-      <div class="header_container_form">
-        <!--LOGO-->
-        <div class="header__logo">
-          <img src="#" alt="#" srcset="" />
-        </div>
-        <!--LOGIN /   LANGUAGE / REGISTRATION   -  form -->
-        <div class="header__form">
-          <!--registration-->
-          <div class="header__form_registration">Registration</div>
-          <!--login-->
-          <div class="header__form_login">Login</div>
-          <!--language-->
-          <div class="header__form_language">AZ</div>
-        </div>
+<!--* верхний header с логин регистрацией и языком и логотипом-->
+<div class="header__wrapper">
+
+
+
+  <!--container UP - LOGO / LOGIN / REgistration / LAnguage-->
+  <div class="header_container_form">
+    <!--LOGO-->
+    <div class="header__logo">
+      <a href="./index.php?source=main">
+        <img src="./style/imgs/logo.png" alt="#" srcset="" />
+      </a>
+    </div>
+    <!--LOGIN /   LANGUAGE / REGISTRATION   -  form -->
+    <div class="header__form">
+      <!-- Add buisness -->
+      <div class="header__add_buisness">
+      <a href="#">
+        <img src="./style/imgs/add.png" alt="" srcset="">
+
+          <p>Biznesi elave edin</p>
+        </a>
       </div>
-      <!--container DOWN - smal navigation-->
-      <div class="header_container_nav">
-        <div class="header_nav_wrap">
-          <!--navigation element-->
-          <div class="header_nav_element">Element 1</div>
-          <!--navigation element-->
-          <div class="header_nav_element">Element 2</div>
-          <!--navigation element-->
-          <div class="header_nav_element">Element 3</div>
-          <!--navigation element-->
-          <div class="header_nav_element">Element 4</div>
-          <!--navigation element-->
-          <div class="header_nav_element">Element 5</div>
-        </div>
+      <!--registration
+      <div class="header__form_registration">
+        
+      <a href="#">
+          <p>Registrasiya</p>
+        </a>
+      </div> -->
+      <!--login-->
+      <div class="header__form_login">
+      <a href="#">
+          <p>Daxil ol</p>
+        </a>
+      </div>
+      <!--language-->
+      <div class="header__form_language">AZ</div>
+    </div>
+  </div>
+
+
+
+
+
+
+  <!--container DOWN - smal navigation-->
+  <div class="header_container_nav">
+    <div class="header_nav_wrap">
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="https://placehold.jp/20x20.png" alt="" srcset="">
+        <a href="#">Francayzinqler</a>
+      </div>
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="https://placehold.jp/20x20.png" alt="" srcset="">
+        <a href="#">Investisiyalar</a>
+      </div>
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="https://placehold.jp/20x20.png" alt="" srcset="">
+        <a href="#">Hazir biznes</a>
+      </div>
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="https://placehold.jp/20x20.png" alt="" srcset="">
+        <a href="#">Avadanliqlarin satisi</a>
       </div>
     </div>
+  </div>
+
+
+
+</div>
