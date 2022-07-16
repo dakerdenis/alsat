3300ebe8e1044e6fdd990f104a08002103db96f8
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-17 00:37:16 +0400

    add

--- includes/search.php
+++ includes/search.php
@@ -1,41 +1,72 @@
 <style>
   <?php
-    include './search.css'
+  include './search.css'
   ?>
 </style>
 <!--*search  контейнер с формой поиска-->
-  <div class="search__wrapper">
+<div class="search__wrapper">
 
 
-    <!--container DOWN - smal navigation-->
-    <div class="header_container_nav">
-      <div class="header_nav_wrap">
-        <!--navigation element-->
-        <div class="header_nav_element">
-          <img src="./style/imgs/franceise.svg" alt="" srcset="">
-          <a href="#">Francayzinqler</a>
-        </div>
-        <!--navigation element-->
-        <div class="header_nav_element">
-          <img src="./style/imgs/invest.svg" alt="" srcset="">
-          <a href="#">Investisiyalar</a>
-        </div>
-        <!--navigation element-->
-        <div class="header_nav_element">
-          <img src="./style/imgs/ready_buisness.svg" alt="" srcset="">
-          <a href="#">Hazir biznes</a>
-        </div>
-        <!--navigation element-->
-        <div class="header_nav_element">
-          <img src="./style/imgs/equip.svg" alt="" srcset="">
-          <a href="#">Avadanliqlarin satisi</a>
-        </div>
+  <!--container DOWN - smal navigation-->
+  <div class="header_container_nav">
+    <div class="header_nav_wrap">
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="./style/imgs/franceise.svg" alt="" srcset="">
+        <a href="#">Francayzinqler</a>
       </div>
-    </div>
-
-    <div class="search_container">
-      <div class="search_name_desc">
-        Hazır biznes almaq və satmaq | Satış üçün biznes təklifi
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="./style/imgs/invest.svg" alt="" srcset="">
+        <a href="#">Investisiyalar</a>
+      </div>
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="./style/imgs/ready_buisness.svg" alt="" srcset="">
+        <a href="#">Hazir biznes</a>
+      </div>
+      <!--navigation element-->
+      <div class="header_nav_element">
+        <img src="./style/imgs/equip.svg" alt="" srcset="">
+        <a href="#">Avadanliqlarin satisi</a>
       </div>
     </div>
   </div>
+
+  <div class="search_container">
+    <div class="search_name_desc">
+      Hazır biznes almaq və satmaq | Satış üçün biznes təklifi
+    </div>
+    <div class="search_block_wrapper">
+      <!--Стандартный блок на 2 элемента--->
+      <div class="search__block_block">
+        <div class="serach__block__almaq">
+          Almaq
+        </div>
+        <div class="serach__block__category">
+          Kateqoriyalar
+        </div>
+      </div>
+      <!--Стандартный блок на 1 элемент--->
+      <div class="search__block_block2">
+        Metro Rayon Küçə
+      </div>
+
+
+      <!--Стандартный блок на 2 элемента--->
+      <div class="search__block_block">
+        <div class="serach__block__buy">
+          Sahə 
+        </div>
+      </div>
+      <!--Стандартный блок на 2 элементa 2--->
+      <div class="search__block_block2-2">
+        <div class="serach__block__price"></div>
+      </div>
+      <!--Блок с вариантами локацией и кнопкой поиска -->
+      <div class="search__block__map_button">
+
+      </div>
+    </div>
+  </div>
+</div>
