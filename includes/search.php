de0bd6c97db193f2a36b3920e8546766c811e2d2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-19 00:53:27 +0400

    add

--- includes/search.php
+++ includes/search.php
@@ -123,7 +123,16 @@
             </div>
             <!---Наличие --->
             <div class="search__block__active">
-              Qiymət
+              <p>Status</p>
+            <div class="custom-select3">
+              <select>
+                <option value="0">Aktiv</option>
+                <option value="0">Aktiv</option>
+                <option value="0">Baglidir</option>
+                <option value="1">Planda</option>
+                <option value="2">Snos</option>
+              </select>
+            </div>
             </div>
           </div>
         </div>
@@ -331,6 +340,91 @@
   then close all select boxes:*/
   document.addEventListener("click", closeAllSelect);
 
+
+  /**********Скрипт для выбора варианта статуса ************** */
+  /**********Скрипт для выбора варианта статуса ************** */
+  /**********Скрипт для выбора варианта статуса ************** */
+  /**********Скрипт для выбора варианта статуса ************** */
+  /**********Скрипт для выбора варианта статуса ************** */
+  var x, i, j, l, ll, selElmnt, a, b, c;
+  /*look for any elements with the class "custom-select":*/
+  x = document.getElementsByClassName("custom-select3");
+  l = x.length;
+  for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected3");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items3 select-hide3");
+    for (j = 1; j < ll; j++) {
+      /*for each option in the original select element,
+      create a new DIV that will act as an option item:*/
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function(e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected3");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected3");
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function(e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect3(this);
+      this.nextSibling.classList.toggle("select-hide3");
+      this.classList.toggle("select-arrow-active3");
+    });
+  }
+
+  function closeAllSelect3(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items3");
+    y = document.getElementsByClassName("select-selected3");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i)
+      } else {
+        y[i].classList.remove("select-arrow-active3");
+      }
+    }
+    for (i = 0; i < xl; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide3");
+      }
+    }
+  }
+  /*if the user clicks anywhere outside the select box,
+  then close all select boxes:*/
+  document.addEventListener("click", closeAllSelect3);
 
 
 
