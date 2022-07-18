00799b6b5ebf838630b2018f69e82c111d8f6a5f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-18 23:27:17 +0400

    add

--- includes/search.php
+++ includes/search.php
@@ -37,7 +37,7 @@
     <div class="search_name_desc">
       Hazır biznes almaq və satmaq &nbsp; | &nbsp; Satış üçün biznes təklifi
     </div>
-    <form class="searc_form" action="">
+    <form class="searc_form" id="my-form" action="">
       <div class="search_block_wrapper">
         <div class="search__block_container">
           <!--Стандартный блок на 2 элемента--->
@@ -51,10 +51,58 @@
           </div>
           <!--Стандартный блок на 1 элемент--->
           <div class="search__block_block2">
-            Metro Rayon Küçə
+            <!-- ввод поиска -->
+            <div class="search_buy_input">
+              <input class="search_buy_input-input" type="text" name="" id="" placeholder="Almaq">
+            </div>
+            <!--выбор станции метро или ориентира-->
+            <div class="search_buy_destination">
+              <span>Metro</span>
+              <span>Rayon</span>
+              <span>Küçə</span>
+            </div>
           </div>
         </div>
+        <div class="search__block_container search__block_container2">
+          <!-- Площадь -->
+          <div class="search__block_block">
+            <div class="search__block__square">
+              <p>Sahə</p>
+              <!--минимальная площадь--->
+              <div class="search__square__min">
+                <input type="number" placeholder="min">
+                <p>m<sup>2</sup></p>
+              </div>
+              <!--максимальная площадь-->
+              <div class="search__square__max">
+                <input type="number" placeholder="max">
+                <p>m<sup>2</sup></p>
+              </div>
+            </div>
+          </div>
+          <!-- Цена и наличие -->
+          <div class="search__block_block3">
+            <!--Цена-->
+            <div class="search__block__price">
+              <p class="price__name_search">Qiymət</p>
+              <!--минимальная цена-->
+              <div class="search__price__min">
+                <input type="number" placeholder="min">
+                <p>azn</p>
+              </div>
 
+              <!--максимальная цена-->
+              <div class="search__price__max">
+                <input type="number" placeholder="max">
+                <p>azn</p>
+              </div>
+            </div>
+            <!---Наличие --->
+            <div class="search__block__active">
+              Qiymət
+            </div>
+          </div>
+        </div>
 
 
 
@@ -65,7 +113,7 @@
             <div class="search__block__additional_element">
               Ətraflı axtarış
             </div>
-            <div class="search__block__additional_element">
+            <div class="search__block__additional_element clear_form" style="color: #EB1B3D;">
               Hamısını sıfırla
             </div>
             <div class="search__block__additional_element">
@@ -78,15 +126,25 @@
             <!---локация на карте--->
             <div class="search__block__map">
               <img src="./style/imgs/location.png" alt="#">
-              Xətirə baxın
+              <p>Xəritədə baxın</p>
             </div>
             <!-----кнопка поиска-->
             <div class="search__block__button">
               <button><span>12</span> Təklifi göstərin</button>
             </div>
+
           </div>
         </div>
       </div>
     </form>
   </div>
 </div>
+<script>
+  /************Скрипт очищающий форму от надписей**************** */
+  let btnClear = document.querySelector('.clear_form');
+  let form = document.querySelector('form');
+
+  btnClear.addEventListener('click', function() {
+    form.reset();
+  });
+</script>
