876a26d599ca7f90d07cbd38f8f3676e57e5ead2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-20 08:12:05 +0400

    add

--- includes/header.php
+++ includes/header.php
@@ -16,56 +16,71 @@
           <span></span>
         </div>
         <div class="menu__body">
-            <div class="menu__wrapper">
-                        <!--Смена языка-->
-          <div class="header__language_button">
-              <div class="header__language"></div>
-              <div class="header_button">TEST</div>
-          </div>
-          <ul class="menu__list">
-
-            <!--ссылка-->
-            <li>
-              <a href="#" class="menu__link">
-                <img src="" alt="" class="menu__link-image">  
-              Lorem Ipsum 1</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 2</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 3 </a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 4</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 5</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 6</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 7</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 8</a>
-            </li>
-                        <!--ссылка-->
-                        <li>
-              <a href="#" class="menu__link">Lorem Ipsum 9</a>
-            </li>
-
-
-          </ul>
+          <div class="menu__wrapper">
+            <!--Смена языка-->
+            <div class="header__language_button">
+              <div class="header__language">
+                <!--RU-->
+                <div class="header_language_change">
+                  RU
+                </div>
+                <!--EN-->
+                <div class="header_language_change">
+                  EN
+                </div>
+                <!--AZ-->
+                <div class="header_language_change">
+                    AZ
+                </div>
+              </div>
+              <div class="header_button">
+                <img class="header_button-img" src="./style/imgs/cancel.png" alt="" srcset="">
+              </div>
             </div>
+            <ul class="menu__list">
+
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">
+                  <img src="" alt="" class="menu__link-image">
+                  Lorem Ipsum 1</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 2</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 3 </a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 4</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 5</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 6</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 7</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 8</a>
+              </li>
+              <!--ссылка-->
+              <li>
+                <a href="#" class="menu__link">Lorem Ipsum 9</a>
+              </li>
+
+
+            </ul>
+          </div>
         </div>
       </a>
       <!--//////////////////////////////////-->
@@ -102,8 +117,14 @@
           <p>Daxil ol</p>
         </a>
       </div>
-      <!--language-->
-      <div class="header__form_language">AZ</div>
+      <!--Язык-->
+      <div class="header__form_language">
+        <button onclick="language()" class="dropbtn_language">AZ</button>
+        <div id="myDropdown_language" class="dropdown-content_language">
+          <a href="#">RU</a>
+          <a href="#">EN</a>
+        </div>
+      </div>
     </div>
 
 
@@ -112,3 +133,22 @@
   </div>
 </div>
 
+
+<script>
+  /********Скрипт языка ********** */
+  function language() {
+    document.getElementById("myDropdown_language").classList.toggle("show_language");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn_language')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content_language");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show_language')) {
+          openDropdown.classList.remove('show_language');
+        }
+      }
+    }
+  }
+</script>
