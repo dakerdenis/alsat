1900e7fc90373c9223951e7c9d0b3d9ccafdeef6
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-22 18:16:17 +0400

    add

--- content/add.php
+++ content/add.php
@@ -6,4 +6,24 @@
     <div class="add_buisness_name">
         Elan yerləşdir
     </div>
+
+    <div class="add_buisness_container">
+        <form action="">
+            <!--wrapper-->
+            <div class="add_buisness_container_wrapper">
+                <!-----1й контейнер --->
+                <div class="add_buisness_container_1">
+
+                </div>
+                <!-----2й контейнер --->
+                <div class="add_buisness_container_1">
+
+                </div>
+            </div>
+            <!--submit-->
+            <div class="add_buisness_container-button">
+                <button type="submit">Yerləşdirmək</button>
+            </div>
+        </form>
+    </div>
 </div>
