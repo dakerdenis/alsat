a770cb2b755756c838bc8a54d3bb58135c0738e7
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-22 22:34:46 +0400

    адд

--- content/add.php
+++ content/add.php
@@ -98,6 +98,29 @@
                             </div>
                         </div>
                     </div>
+
+                    <!---Категория-->
+                    <div class="add_buisness__element">
+                        <div class="add_buisness__element-desc">
+                            Kategoriya*
+                        </div>
+                        <div class="add_buisness__element-input">
+                            <!--категории--->
+                            <div class="custom-select_category">
+                                <select>
+                                    <option value="0">Kategoriya</option>
+                                    <option value="0">Kategoriya</option>
+                                    <option value="1">Kafe</option>
+                                    <option value="2">Restoran</option>
+                                    <option value="3">Aptek</option>
+                                    <option value="4">Nalogovaya</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
             <!--submit-->
@@ -289,6 +312,91 @@
         for (i = 0; i < xl; i++) {
             if (arrNo.indexOf(i)) {
                 x[i].classList.add("select-hide_goal");
+            }
+        }
+    }
+    /*if the user clicks anywhere outside the select box,
+    then close all select boxes:*/
+    document.addEventListener("click", closeAllSelect1);
+
+
+    /**********Скрипт для выбора цели продажи ************** */
+    /**********Скрипт для выбора цели продажи ************** */
+    /**********Скрипт для выбора цели продажи ************** */
+    /**********Скрипт для выбора цели продажи ************** */
+    var x, i, j, l, ll, selElmnt, a, b, c;
+    /*look for any elements with the class "custom-select":*/
+    x = document.getElementsByClassName("custom-select_category");
+    l = x.length;
+    for (i = 0; i < l; i++) {
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        ll = selElmnt.length;
+        /*for each element, create a new DIV that will act as the selected item:*/
+        a = document.createElement("DIV");
+        a.setAttribute("class", "select-selected_category");
+        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+        x[i].appendChild(a);
+        /*for each element, create a new DIV that will contain the option list:*/
+        b = document.createElement("DIV");
+        b.setAttribute("class", "select-items_goal select-hide_category");
+        for (j = 1; j < ll; j++) {
+            /*for each option in the original select element,
+            create a new DIV that will act as an option item:*/
+            c = document.createElement("DIV");
+            c.innerHTML = selElmnt.options[j].innerHTML;
+            c.addEventListener("click", function(e) {
+                /*when an item is clicked, update the original select box,
+                and the selected item:*/
+                var y, i, k, s, h, sl, yl;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                sl = s.length;
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < sl; i++) {
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected_category");
+                        yl = y.length;
+                        for (k = 0; k < yl; k++) {
+                            y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected_category");
+                        break;
+                    }
+                }
+                h.click();
+            });
+            b.appendChild(c);
+        }
+        x[i].appendChild(b);
+        a.addEventListener("click", function(e) {
+            /*when the select box is clicked, close any other select boxes,
+            and open/close the current select box:*/
+            e.stopPropagation();
+            closeAllSelect1(this);
+            this.nextSibling.classList.toggle("select-hide_category");
+            this.classList.toggle("select-arrow-active_category");
+        });
+    }
+
+    function closeAllSelect1(elmnt) {
+        /*a function that will close all select boxes in the document,
+        except the current select box:*/
+        var x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items_category");
+        y = document.getElementsByClassName("select-selected_category");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+                arrNo.push(i)
+            } else {
+                y[i].classList.remove("select-arrow-active_category");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide_category");
             }
         }
     }
