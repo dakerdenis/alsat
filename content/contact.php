11f58ceaaad29820e5f75f95dc7345216cbb84e3
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-27 13:33:05 +0400

    addd

--- content/contact.php
+++ content/contact.php
@@ -1,6 +1,9 @@
 <style>
     <?php include './content/contact.css'; ?>
 </style>
+<style>
+    <?php include './content/login.css'; ?>
+</style>
 
 <div class="contact__wrapper">
     <div class="contact__desc">
@@ -8,19 +11,46 @@
     </div>
     <div class="contact__container">
         <form action="">
-                    <!--Связаться с  нами описание--->
-        <div class="contact__block__desc">
-            <div class="contact__block__desc_name">
-                Мы всегда рады помочь нашим клиентам !
+            <!--Связаться с  нами описание--->
+            <div class="contact__block__desc">
+                <div class="contact__block__desc_name">
+                    Мы всегда рады помочь нашим клиентам !
+                </div>
+                <div class="contact__block__desc_text">
+                    Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
+                </div>
             </div>
-            <div class="contact__block__desc_text">
-                Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
+            <!----Связаться с нами форма запроса -->
+            <div class="contact__block__from_content">
+                    <!--name-->
+                    <div class="log_reg_element__wrapper">
+                        <div class="log_reg_element_wraper_desc">
+                            Ad soyad*
+                        </div>
+                        <div class="log_reg_element_wraper_input">
+                            <input class="log_reg_element_wraper_input-input" type="text">
+                        </div>
+                    </div>
+                    <!--emaiL-->
+                    <div class="log_reg_element__wrapper">
+                        <div class="log_reg_element_wraper_desc">
+                            E-mail*
+                        </div>
+                        <div class="log_reg_element_wraper_input">
+                            <input class="log_reg_element_wraper_input-input" type="text">
+                        </div>
+                    </div>
+                    <!--Telefon-->
+                    <div class="log_reg_element__wrapper">
+                        <div class="log_reg_element_wraper_desc">
+                            Telefon*
+                        </div>
+                        <div class="log_reg_element_wraper_input">
+                            <input class="log_reg_element_wraper_input-input" type="number">
+                        </div>
+                    </div>
+                    <!---message--->
             </div>
-        </div>
-        <!----Связаться с нами форма запроса -->
-        <div class="contact__block__from_content">
-            
-        </div>
         </form>
     </div>
 </div>
