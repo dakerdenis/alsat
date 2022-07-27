c342241562391af9281fe1959df18eacff508f1c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-27 13:31:48 +0400

    add

--- content/contact.php
+++ content/contact.php
@@ -1,12 +1,28 @@
 <style>
-    <?php include './content/page.css'; ?>
+    <?php include './content/contact.css'; ?>
 </style>
 
-<div class="page__wrapper">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, praesentium quas cumque expedita sapiente nemo provident laboriosam ipsum nobis inventore, molestiae molestias! Ducimus commodi porro praesentium. Quasi modi ratione accusantium!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex non, aliquid, ipsa labore fugiat nemo pariatur beatae voluptates consectetur aliquam magnam minima et adipisci sit, consequuntur voluptatem quia harum quis.
-    Architecto minus fuga exercitationem consectetur error, id officia nisi quam, sunt, maxime omnis debitis eum. Deleniti assumenda ad itaque quas, odit vero quo aliquid eaque? Facere ipsa doloremque earum ea.
-    <br><br><br><br><br><br><br><br><br><br> СВЯЗЬ С НАМИ
+<div class="contact__wrapper">
+    <div class="contact__desc">
+        Bizimle Elage
+    </div>
+    <div class="contact__container">
+        <form action="">
+                    <!--Связаться с  нами описание--->
+        <div class="contact__block__desc">
+            <div class="contact__block__desc_name">
+                Мы всегда рады помочь нашим клиентам !
+            </div>
+            <div class="contact__block__desc_text">
+                Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
+            </div>
+        </div>
+        <!----Связаться с нами форма запроса -->
+        <div class="contact__block__from_content">
+            
+        </div>
+        </form>
+    </div>
 </div>
 
 
