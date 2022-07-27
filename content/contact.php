91ce1a0417e0cd40fd206f02f817acc7da60a50f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-27 14:07:47 +0400

    add

--- content/contact.php
+++ content/contact.php
@@ -9,39 +9,108 @@
     <div class="contact__desc">
         Bizimle Elage
     </div>
+    <!------desc --->
     <div class="contact__text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae ducimus tenetur quas tempora, ipsum impedit quo nisi molestiae iusto nulla reprehenderit doloremque iste necessitatibus numquam voluptatem eos! Ad, distinctio!
         Eligendi repellendus perspiciatis molestias vitae id amet unde pariatur ducimus, eos dignissimos reprehenderit modi sed, adipisci labore inventore voluptate accusantium. Vel hic dicta dolores, deleniti recusandae inventore voluptates repellendus eveniet!
     </div>
-
+    <!---contact Info--->
     <div class="contact__info__wrapper">
+        <!--- NUmbers---->
         <div class="contact__info__wrapper_block">
             <div class="contact__info__wrapper_block_name">
                 Nomreler
             </div>
             <p class="contact__bumber__p">
-            моб: +55 (855) 845 - 55 - 55
+                моб: +55 (855) 845 - 55 - 55
             </p>
             <p class="contact__bumber__p">
-            дом: +55 (915) 888 - 58 - 87
+                дом: +55 (915) 888 - 58 - 87
             </p>
         </div>
-
+        <!--Adresses--->
         <div class="contact__info__wrapper_block">
             <div class="contact__info__wrapper_block_name">
                 Adresler
             </div>
             <p class="contact__adress__p">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             </p>
         </div>
-
+        <!----Social networks--->
         <div class="contact__info__wrapper_block">
             <div class="contact__info__wrapper_block_name">
                 Social networks
             </div>
-            .contact__social_symbols
+            <div class="contact__social_symbols">
+                <a href="">
+                    <img src="./style/imgs/social.svg" alt="" srcset="">
+                </a>
+                <a href="">
+                    <img src="./style/imgs/social.svg" alt="" srcset="">
+                </a>
+                <a href="">
+                    <img src="./style/imgs/social.svg" alt="" srcset="">
+                </a>
+                <a href="">
+                    <img src="./style/imgs/social.svg" alt="" srcset="">
+                </a>
+            </div>
         </div>
+    </div>
+
+
+    <!----contact form ---->
+    <div class="contact__form_container">
+        <form action="">
+            <!-----name and number -->
+            <div class="two_elements__from_container">
+                <!--name-->
+                <div class="log_reg_element__wrapper">
+                    <div class="log_reg_element_wraper_desc">
+                        Ad soyad*
+                    </div>
+                    <div class="log_reg_element_wraper_input">
+                        <input class="log_reg_element_wraper_input-input" type="text">
+                    </div>
+                </div>
+                <!--number-->
+                <div class="log_reg_element__wrapper">
+                    <div class="log_reg_element_wraper_desc">
+                        Number*
+                    </div>
+                    <div class="log_reg_element_wraper_input">
+                        <input class="log_reg_element_wraper_input-input" type="number">
+                    </div>
+                </div>
+            </div>
+            <!----email and task --->
+            <div class="two_elements__from_container">
+                <!--name-->
+                <div class="log_reg_element__wrapper">
+                    <div class="log_reg_element_wraper_desc">
+                        Ad soyad*
+                    </div>
+                    <div class="log_reg_element_wraper_input">
+                        <input class="log_reg_element_wraper_input-input" type="text">
+                    </div>
+                </div>
+                <!--emaiL-->
+                <div class="log_reg_element__wrapper">
+                    <div class="log_reg_element_wraper_desc">
+                        E-mail*
+                    </div>
+                    <div class="log_reg_element_wraper_input">
+                        <input class="log_reg_element_wraper_input-input" type="email">
+                    </div>
+                </div>
+            </div>
+            <textarea class="form__textarea__container"></textarea>
+
+            <div class="contact__form__button__submit">
+                <button>Send MEssage</button>
+            </div>
+        </form>
     </div>
 </div>
 
