1996606da7998218d16f6bce5ecbcc468c544700
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-27 13:55:24 +0400

    add

--- content/contact.php
+++ content/contact.php
@@ -9,69 +9,40 @@
     <div class="contact__desc">
         Bizimle Elage
     </div>
-    <form action="">
-        <div class="contact__container">
+    <div class="contact__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae ducimus tenetur quas tempora, ipsum impedit quo nisi molestiae iusto nulla reprehenderit doloremque iste necessitatibus numquam voluptatem eos! Ad, distinctio!
+        Eligendi repellendus perspiciatis molestias vitae id amet unde pariatur ducimus, eos dignissimos reprehenderit modi sed, adipisci labore inventore voluptate accusantium. Vel hic dicta dolores, deleniti recusandae inventore voluptates repellendus eveniet!
+    </div>
 
-            <div class="contact__container__wrapper">
-                <!--Связаться с  нами описание--->
-                <div class="contact__block__desc">
-                    <div class="contact__block__desc_name">
-                        Мы всегда рады помочь нашим клиентам !
-                    </div>
-                    <div class="contact__block__desc_text">
-                        Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
-                    </div>
-                </div>
-                <!----Связаться с нами форма запроса -->
-                <div class="contact__block__from_content">
-                    <!--name-->
-                    <div class="log_reg_element__wrapper">
-                        <div class="log_reg_element_wraper_desc">
-                            Ad soyad*
-                        </div>
-                        <div class="log_reg_element_wraper_input">
-                            <input class="log_reg_element_wraper_input-input" type="text">
-                        </div>
-                    </div>
-                    <!--emaiL-->
-                    <div class="log_reg_element__wrapper">
-                        <div class="log_reg_element_wraper_desc">
-                            E-mail*
-                        </div>
-                        <div class="log_reg_element_wraper_input">
-                            <input class="log_reg_element_wraper_input-input" type="text">
-                        </div>
-                    </div>
-                    <!--Telefon-->
-                    <div class="log_reg_element__wrapper">
-                        <div class="log_reg_element_wraper_desc">
-                            Telefon*
-                        </div>
-                        <div class="log_reg_element_wraper_input">
-                            <input class="log_reg_element_wraper_input-input" type="number">
-                        </div>
-                    </div>
-                    <!---message--->
-                    <div class="log_reg_element__wrapper">
-                        <div class="log_reg_element_wraper_desc">
-                            Mesaj
-                        </div>
-                        <div class="log_reg_element_wraper_input">
-                            <textarea></textarea>
-                        </div>
-                    </div>
-                </div>
+    <div class="contact__info__wrapper">
+        <div class="contact__info__wrapper_block">
+            <div class="contact__info__wrapper_block_name">
+                Nomreler
             </div>
-
-
-
-            <div class="contact__block_form_button">
-            <button type="submit">Göndər</button>
+            <p class="contact__bumber__p">
+            моб: +55 (855) 845 - 55 - 55
+            </p>
+            <p class="contact__bumber__p">
+            дом: +55 (915) 888 - 58 - 87
+            </p>
         </div>
 
+        <div class="contact__info__wrapper_block">
+            <div class="contact__info__wrapper_block_name">
+                Adresler
+            </div>
+            <p class="contact__adress__p">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            </p>
         </div>
 
-    </form>
+        <div class="contact__info__wrapper_block">
+            <div class="contact__info__wrapper_block_name">
+                Social networks
+            </div>
+            .contact__social_symbols
+        </div>
+    </div>
 </div>
 
 
