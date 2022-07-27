376c3bd90a39c6c9dd010fbdd162852bb8d9d193
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-07-27 13:48:29 +0400

    add

--- content/contact.php
+++ content/contact.php
@@ -10,19 +10,20 @@
         Bizimle Elage
     </div>
     <form action="">
-    <div class="contact__container">
+        <div class="contact__container">
 
-            <!--Связаться с  нами описание--->
-            <div class="contact__block__desc">
-                <div class="contact__block__desc_name">
-                    Мы всегда рады помочь нашим клиентам !
+            <div class="contact__container__wrapper">
+                <!--Связаться с  нами описание--->
+                <div class="contact__block__desc">
+                    <div class="contact__block__desc_name">
+                        Мы всегда рады помочь нашим клиентам !
+                    </div>
+                    <div class="contact__block__desc_text">
+                        Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
+                    </div>
                 </div>
-                <div class="contact__block__desc_text">
-                    Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veritatis incidunt beatae rerum explicabo cumque, sapiente, inventore praesentium ex nam cum doloremque consequatur rem saepe atque similique illo sed. Eligendi. adipisicing elit. Veritatis suscipit accusamus repellendus autem, amet nobis nihil quam earum asperiores dicta alias, cum magnam cumque. Illo at nisi quam quas tempore.
-                </div>
-            </div>
-            <!----Связаться с нами форма запроса -->
-            <div class="contact__block__from_content">
+                <!----Связаться с нами форма запроса -->
+                <div class="contact__block__from_content">
                     <!--name-->
                     <div class="log_reg_element__wrapper">
                         <div class="log_reg_element_wraper_desc">
@@ -59,12 +60,17 @@
                             <textarea></textarea>
                         </div>
                     </div>
+                </div>
             </div>
 
-    </div>
-    <div class="contact__block_form_button">
-        <button type="submit">Göndər</button>
-    </div>
+
+
+            <div class="contact__block_form_button">
+            <button type="submit">Göndər</button>
+        </div>
+
+        </div>
+
     </form>
 </div>
 
