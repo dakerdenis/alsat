873ef7f60c3c8aaaff12c0492d7fd26ead81dd46
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-06 15:04:33 +0400

    add

--- content/account__page/popup__top.php
+++ content/account__page/popup__top.php
@@ -19,7 +19,10 @@
                             <div class="popup__content__block-block">
                                 <!--название категории вывода в топ--->
                                 <div class="popup__content__block__title">
-                                    Поднять в топ
+                                    Поднять в общий топ
+                                </div>
+                                <div class="popup__content__block__title-desc">
+                                    Ваше объявление будет в ТОПе на главной странице
                                 </div>
                                 <!---Кнопки с выбором суммы--->
                                 <div class="popup__content__block_buttons">
@@ -43,7 +46,7 @@
                         </div>
                     </div>
                     <!---Поднять в топ категории---->
-                    <div class="popup__content__block">
+                    <div class="popup__content__block popup__content__block2">
                         <div class="popup__content__block_container">
                             <!---иконка вывода в топ--->
                             <div class="popup__content__block-icon"></div>
@@ -51,7 +54,10 @@
                             <div class="popup__content__block-block">
                                 <!--название категории вывода в топ--->
                                 <div class="popup__content__block__title">
-                                    Поднять в топ
+                                    Поднять в топ категории
+                                </div>
+                                <div class="popup__content__block__title-desc">
+                                    Ваше объявление будет в ТОПе в вашей категории
                                 </div>
                                 <!---Кнопки с выбором суммы--->
                                 <div class="popup__content__block_buttons">
@@ -83,7 +89,10 @@
                             <div class="popup__content__block-block">
                                 <!--название категории вывода в топ--->
                                 <div class="popup__content__block__title">
-                                    Поднять в топ
+                                    Поднять выше
+                                </div>
+                                <div class="popup__content__block__title-desc">
+                                    Ваше объявление поднимается в результатах поиска
                                 </div>
                                 <!---Кнопки с выбором суммы--->
                                 <div class="popup__content__block_buttons">
