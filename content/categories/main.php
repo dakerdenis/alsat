274413d0a9167345ad8d7be52201de148a13ae8f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-09 13:52:42 +0400

    add

---
+++ content/categories/main.php
@@ -0,0 +1,98 @@
+    <!-------BIZNES CATEGORIES---------->
+    <div class="content__wrapper">
+        <!-- контейнер по 2 штуки-->
+        <div class="content_wrapper_4elements">
+
+
+            <!-- 2 элемента контейнер -->
+            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
+                <!--Block name -->
+                <div class="four__block_buisness__name">
+                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
+                    <p class="four__block_buisness__name-p">Biznes satışı</p>
+                </div>
+                <!-- 2 elements -->
+                <div class="content_wrapper_2elements-buisness2">
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                </div>
+
+                <!--show more button -->
+                <div class="sell_find_buisness_button">
+                    <a href="#">Növbəti təklifləri göstər</a>
+                </div>
+            </div>
+
+
+            <!-- 2 элемента контейнер -->
+            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
+                <!--Block name -->
+                <div class="four__block_buisness__name">
+                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
+                    <p class="four__block_buisness__name-p">Franşiza satışı</p>
+                </div>
+                <!-- 2 elements -->
+                <div class="content_wrapper_2elements-buisness2">
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                </div>
+
+                <!--show more button -->
+                <div class="sell_find_buisness_button">
+                    <a href="#">Növbəti təklifləri göstər</a>
+                </div>
+            </div>
+        </div>
+        <!-- контейнер по 2 штуки-->
+        <div class="content_wrapper_4elements">
+
+
+            <!-- 2 элемента контейнер -->
+            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
+                <!--Block name -->
+                <div class="four__block_buisness__name">
+                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
+                    <p class="four__block_buisness__name-p">Biznes axtarıram</p>
+                </div>
+                <!-- 2 elements -->
+                <div class="content_wrapper_2elements-buisness2">
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                    <!--element block -->
+                    <?php include './content/block.php'; ?>
+                </div>
+
+                <!--show more button -->
+                <div class="sell_find_buisness_button">
+                    <a href="#">Növbəti təklifləri göstər</a>
+                </div>
+            </div>
+
+
+            <!-- 2 элемента контейнер -->
+            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
+                <!--Block name -->
+                <div class="four__block_buisness__name">
+                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
+                    <p class="four__block_buisness__name-p">Biznes üçün avadanlıq satışı</p>
+                </div>
+                <!-- 2 elements -->
+                <div class="content_wrapper_2elements-buisness2">
+                    <!--element block -->
+                    <?php include './content/block_avadanliq.php'; ?>
+                    <!--element block -->
+                    <?php include './content/block_avadanliq.php'; ?>
+                </div>
+
+                <!--show more button -->
+                <div class="sell_find_buisness_button">
+                    <a href="./index.php?source=main&content=avadanliq">Növbəti təklifləri göstər</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
--- content/main.php
+++ content/main.php
@@ -1,7 +1,8 @@
+
     <!--стили главной страницы-->
     <style>
-        <?php include './content/main.css'; ?><?php include './content/block.css';
-                                                ?>
+        <?php include './content/main.css'; ?>
+        <?php include './content/block.css';?>
     </style>
     <div class="filter__wrapper">
         <div class="filter__container">
@@ -82,105 +83,31 @@
             <img class="advertisement__img-img" src="./style/imgs/advertisement_samll.png" alt="">
         </div>
     </div>
-
-    <!-------BIZNES CATEGORIES---------->
-    <div class="content__wrapper">
-        <!-- контейнер по 2 штуки-->
-        <div class="content_wrapper_4elements">
-
-
-            <!-- 2 элемента контейнер -->
-            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
-                <!--Block name -->
-                <div class="four__block_buisness__name">
-                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
-                    <p class="four__block_buisness__name-p">Biznes satışı</p>
-                </div>
-                <!-- 2 elements -->
-                <div class="content_wrapper_2elements-buisness2">
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                </div>
-
-                <!--show more button -->
-                <div class="sell_find_buisness_button">
-                    <a href="#">Növbəti təklifləri göstər</a>
-                </div>
-            </div>
+        <!----Тут контент меняется ---->
+        <?php
+        if(isset($_GET['content'])){
+            $main_content = $_GET['content'];
 
 
-            <!-- 2 элемента контейнер -->
-            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
-                <!--Block name -->
-                <div class="four__block_buisness__name">
-                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
-                    <p class="four__block_buisness__name-p">Franşiza satışı</p>
-                </div>
-                <!-- 2 elements -->
-                <div class="content_wrapper_2elements-buisness2">
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                </div>
+        } else {
+            $main_content = '';
+        }
+        switch($main_content){
+            case 'avadanliq';
+            include './content/categories/categories.php';
+            break;
+            
+            case 'biznes_satish';
+            include './content/categories/search_elements.php';
+            break;
+            
 
-                <!--show more button -->
-                <div class="sell_find_buisness_button">
-                    <a href="#">Növbəti təklifləri göstər</a>
-                </div>
-            </div>
-        </div>
-        <!-- контейнер по 2 штуки-->
-        <div class="content_wrapper_4elements">
+            default:
+            include './content/categories/main.php';
+            break;
+        }
+    ?>
 
-
-            <!-- 2 элемента контейнер -->
-            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
-                <!--Block name -->
-                <div class="four__block_buisness__name">
-                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
-                    <p class="four__block_buisness__name-p">Biznes axtarıram</p>
-                </div>
-                <!-- 2 elements -->
-                <div class="content_wrapper_2elements-buisness2">
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                    <!--element block -->
-                    <?php include './content/block.php'; ?>
-                </div>
-
-                <!--show more button -->
-                <div class="sell_find_buisness_button">
-                    <a href="#">Növbəti təklifləri göstər</a>
-                </div>
-            </div>
-
-
-            <!-- 2 элемента контейнер -->
-            <div class="content_wrapper_2elements content_wrapper_2elements-buisness">
-                <!--Block name -->
-                <div class="four__block_buisness__name">
-                    <img src="https://placehold.jp/3d4070/ffffff/28x28.png" alt="" class="four__block_buisness__name-img">
-                    <p class="four__block_buisness__name-p">Biznes üçün avadanlıq satışı</p>
-                </div>
-                <!-- 2 elements -->
-                <div class="content_wrapper_2elements-buisness2">
-                    <!--element block -->
-                    <?php include './content/block_avadanliq.php'; ?>
-                    <!--element block -->
-                    <?php include './content/block_avadanliq.php'; ?>
-                </div>
-
-                <!--show more button -->
-                <div class="sell_find_buisness_button">
-                    <a href="#">Növbəti təklifləri göstər</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
 
     <!-- Advertisement block 2-->
 
