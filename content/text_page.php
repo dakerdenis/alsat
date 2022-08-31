2ad1c2a5a194e72b7ae1aa450c56e1d70b7c352f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-31 23:10:56 +0400

    add
    добавил стили к странице текста

--- content/text_page.php
+++ content/text_page.php
@@ -1,3 +1,24 @@
 <style>
-    <?php include './content/text_page.php'  ?>
+    <?php include './content/text_page.css'  ?>
 </style>
+<div class="textpage__wrapper">
+    <!--SITIMAP NAME---->
+    <div class="textpage__name_wrapper">
+        <!---->
+        <div class="textpage_name_block">
+            <div class="textpage__name">
+                Saytın Xəritəsi
+            </div>
+            <div class="textpage__border"></div>
+        </div>
+        <!---->
+    </div>
+
+
+    <div class="textpage__content__container">
+        
+    </div>
+
+
+
+</div>
