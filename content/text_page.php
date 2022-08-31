b5aed6fa4a660b8aa722dd69dab4eca809967e5c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-31 23:34:10 +0400

    sitemap
    доделал sitemap страницу

--- content/text_page.php
+++ content/text_page.php
@@ -7,7 +7,7 @@
         <!---->
         <div class="textpage_name_block">
             <div class="textpage__name">
-            Istifadəçi Razilaşma
+                Istifadəçi Razilaşma
             </div>
             <div class="textpage__border"></div>
         </div>
@@ -25,8 +25,8 @@
         </div>
 
 
-                <!--Блок текста-->
-                <div class="textpage__textblock">
+        <!--Блок текста-->
+        <div class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea laudantium debitis soluta incidunt nulla tempore, quaerat ipsam, corporis numquam saepe exercitationem enim harum iste reprehenderit magnam adipisci! Deleniti, esse.
             Sit doloremque ipsum, in nobis perferendis hic quibusdam quia aspernatur alias laudantium. Error placeat odio, beatae obcaecati ratione quasi soluta, distinctio enim doloribus reprehenderit commodi nihil pariatur? Quas, dignissimos accusamus?
@@ -35,26 +35,27 @@
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
         </div>
 
-                <!--Блок текста-->
-                <div class="textpage__textblock">
+        <!--Блок текста-->
+        <div class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
-            
+
         </div>
 
-                <!--Блок текста-->
-                <div class="textpage__textblock">
+        <!--Блок текста-->
+        <div class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
         </div>
 
 
-                <!--Блок текста-->
-                <div class="textpage__textblock">
+        <!--Блок текста-->
+        <div class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
-        </div>        <!--Блок текста-->
+        </div>
+        <!--Блок текста-->
         <div class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
