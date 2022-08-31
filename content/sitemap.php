2dbb2679123b6cf352b08d88b96acc3e4a47eb1f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-31 20:16:16 +0400

    fonts
    Добавил шрифты Poppins

--- content/sitemap.php
+++ content/sitemap.php
@@ -5,12 +5,14 @@
 <div class="sitemap__wrapper">
     <!--SITIMAP NAME---->
     <div class="sitemap__name_wrapper">
+        <!---->
         <div class="sitemap_name_block">
             <div class="sitemap__name">
                 Saytın Xəritəsi
             </div>
             <div class="sitemap__border"></div>
         </div>
+        <!---->
     </div>
     <!---sitemap main container -->
     <div class="sitemap__content__wrapper">
