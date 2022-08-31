c913bbf33bd23f060fae534f19f3cee203a3645f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-08-31 22:46:48 +0400

    sitemap
    страница карты сайта

--- content/sitemap.php
+++ content/sitemap.php
@@ -16,6 +16,162 @@
     </div>
     <!---sitemap main container -->
     <div class="sitemap__content__wrapper">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae voluptates explicabo rerum earum animi quidem! Incidunt enim numquam quas dolores eos laudantium repudiandae repellendus? Mollitia esse magnam reprehenderit ipsa.
+        <!--Блок карты сайта 1-->
+        <div class="sitemap__block_">
+            <!--название раздлела карты сайта--->
+            <div class="sitemap__block_name">Fəaliyyət</div>
+            <!----->
+            <div class="sitemap__elements__content">
+
+                <!--Блоки с названиями-->
+                <div class="sitemap_element_container">
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Ümumi məlumat</a>
+                        </div>
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Ipsum dolor</a>
+                        </div>
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Dostlar və əməkdaşlar</a>
+                        </div>
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Lorem</a>
+                        </div>
+                    </div>
+
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Struktur</a>
+                        </div>
+                    </div>
+
+                    <!--Последний элемент-->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line sitemap__elementblock__line_last">
+                            <div class="additional__line additional__line_last "></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Əlaqə</a>
+                        </div>
+                    </div>
+                </div>
+                <!---->
+            </div>
+            <!----->
+        </div>
+
+
+
+
+        <!--Блок карты сайта 2-->
+        <div class="sitemap__block_">
+            <!--название раздлела карты сайта--->
+            <div class="sitemap__block_name">Əməkdaşlıq</div>
+            <!----->
+            <div class="sitemap__elements__content">
+
+                <!--Блоки с названиями-->
+                <div class="sitemap_element_container">
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Ümumi məlumat</a>
+                        </div>
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Ipsum dolor</a>
+                        </div>
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                            <a href="#">Dostlar və əməkdaşlar</a>
+                        </div>
+                    </div>
+                    <!--элемент вложенный---->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Вложенный элемент</a>                        
+                        </div>
+                        
+                        
+                    </div>
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Lorem</a>
+                        </div>
+                    </div>
+
+                    <!--Элемент--->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line">
+                            <div class="additional__line"></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Struktur</a>
+                        </div>
+                    </div>
+
+                    <!--Последний элемент-->
+                    <div class="sitemap__element_block">
+                        <div class="sitemap__elementblock__line sitemap__elementblock__line_last">
+                            <div class="additional__line additional__line_last "></div>
+                        </div>
+                        <div class="sitemap__elementblock__name">
+                        <a href="#">Əlaqə</a>
+                        </div>
+                    </div>
+                </div>
+                <!---->
+            </div>
+            <!----->
+        </div>
+
+
     </div>
 </div>
