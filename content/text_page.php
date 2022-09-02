16cd7c5be6f6c0f528b683796a79d1f7e7c4898f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-02 20:49:51 +0400

    текстовая страница
    заменил блоки тегами p

--- content/text_page.php
+++ content/text_page.php
@@ -18,49 +18,57 @@
     <div class="textpage__content__container">
 
         <!--Блок текста-->
-        <div class="textpage__textblock">
+        <p class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
-        </div>
+        </p>
 
 
         <!--Блок текста-->
-        <div class="textpage__textblock">
+        <p class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea laudantium debitis soluta incidunt nulla tempore, quaerat ipsam, corporis numquam saepe exercitationem enim harum iste reprehenderit magnam adipisci! Deleniti, esse.
             Sit doloremque ipsum, in nobis perferendis hic quibusdam quia aspernatur alias laudantium. Error placeat odio, beatae obcaecati ratione quasi soluta, distinctio enim doloribus reprehenderit commodi nihil pariatur? Quas, dignissimos accusamus?
             Inventore illum suscipit corrupti nam ex reiciendis voluptas, quo vero quisquam dolore at nisi quas, blanditiis odit hic et eius voluptates, similique aliquam dolores autem excepturi laudantium vel. Ex, omnis?
             Expedita magni amet necessitatibus atque quaerat? Ut eaque quasi quae, aspernatur nobis impedit dolores ipsam! Fugiat officia est fuga ut illo architecto in odit laboriosam, explicabo quaerat ducimus unde incidunt. aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
+        </p>
+        <!--Блок картинки--->
+        <div class="textpage__image__block">
+            <img src="./style/imgs/123456.jpg" alt="">
         </div>
 
         <!--Блок текста-->
-        <div class="textpage__textblock">
+        <p class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
 
-        </div>
+        </p>
 
         <!--Блок текста-->
-        <div class="textpage__textblock">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
-            Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
-            Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
-        </div>
-
-
-        <!--Блок текста-->
-        <div class="textpage__textblock">
+        <p class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
-        </div>
+        </p>
+
+
         <!--Блок текста-->
-        <div class="textpage__textblock">
+        <p class="textpage__textblock">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
             Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
             Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
+        </p>
+        <!--Блок картинки--->
+        <div class="textpage__image__block">
+            <img src="./style/imgs/768.png" alt="">
         </div>
+        <!--Блок текста-->
+        <p class="textpage__textblock">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ab earum quibusdam inventore sapiente similique, commodi consequuntur reiciendis maiores aut, assumenda cupiditate perspiciatis reprehenderit voluptates molestias, obcaecati dolor. Quasi, neque!
+            Quisquam laborum est ducimus nostrum accusantium, aspernatur quibusdam optio dolores et impedit! Praesentium, dicta, cumque iusto fugit eos ex autem hic ullam voluptatem quo atque iure maiores nam error veniam!
+            Est iste officia libero. Tempora voluptatem iure fuga maxime ex laboriosam quidem eos hic natus aperiam nisi dolore modi, obcaecati, asperiores neque quae alias dolorem dignissimos quo! Quasi, iusto sunt.
+        </p>
 
     </div>
 </div>
